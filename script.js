6215af9dca83d96b770b82a3e36cf7f4667aa5d1
Daniil Solynin <didhat82@gmail.com>
2025-03-11 21:48:06 +0300

    create page

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('colorButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = document.body.style.backgroundColor === 'lightblue' ? '#f4f4f4' : 'lightblue';
+});
